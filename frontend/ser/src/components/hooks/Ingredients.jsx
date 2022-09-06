1596f1285115672baed6e57729c881b99cb43f8b
elStriker <briankouophe38@gmail.com>
2022-09-06 15:43:07 +0200

    fetching works

--- frontend/ser/src/components/hooks/Ingredients.jsx
+++ frontend/ser/src/components/hooks/Ingredients.jsx
@@ -3,26 +3,40 @@ import { useEffect, useState } from "react";
 
 function Ingredients() {
     const [items, setItems] = useState([]);
-}
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
 
-useEffect(() => {
-    fetch("http://localhost:3000/ingredients")
-    .then(res => res.json())
-    .then(
-        (result) => {
-            setItems(result)
-        }
-    )
-})
 
-if (items) {
-    return<ul>
-        {items.map(item => (
-            <li>
-                {item.name}
-            </li>
-        ))}
-    </ul>
+    useEffect(() => {
+        fetch("http://localhost:8080/ingredients")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                setIsLoaded(true);
+                setItems(result);
+            },
+            (error) => {
+                setIsLoaded(true);
+                setError(error);
+            }
+            )
+        }, [])
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <ul>
+                {items.map(item => (
+                    <li key={item.id}>
+                        {item.name} 
+                    </li>
+                ))}
+            </ul>
+        );
+    }
 }
 
 export default Ingredients
