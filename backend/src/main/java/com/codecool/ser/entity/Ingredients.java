f7a60d4fe6d58961ca56839d6b623e7b5c095ab5
R0-M4N <fannroman850@gmail.com>
2022-09-06 11:46:54 +0200

    Ingredient entity created

---
+++ backend/src/main/java/com/codecool/ser/entity/Ingredients.java
@@ -0,0 +1,16 @@
+package com.codecool.ser.entity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ingredients {
+    public static final Map<String, String> INGREDIENTS = new HashMap<>()
+    {{
+        put("Chicken meat", "Shrimp");
+        put("Pineapple", "Grape");
+        put("Milk", "Cheese");
+        put("Kale", "Bread");
+        put("Cow meat", "Hazelnut");
+    }};
+
+}
