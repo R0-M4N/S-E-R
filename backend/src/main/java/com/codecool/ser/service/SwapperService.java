05f5b276101a5cdfbd84145b96f51604bd8793dc
R0-M4N <fannroman850@gmail.com>
2022-09-06 13:33:49 +0200

    refactored swapping method

--- backend/src/main/java/com/codecool/ser/service/SwapperService.java
+++ backend/src/main/java/com/codecool/ser/service/SwapperService.java
@@ -1,13 +1,15 @@
 package com.codecool.ser.service;
 
 import com.codecool.ser.entity.Ingredients;
+import com.codecool.ser.persistence.entity.Ingredient;
 
 import java.util.Map;
 
 public class SwapperService {
     Map<String, String> ingredientPairs = Ingredients.INGREDIENTS;
 
-    public String swapIngredients(String pickedIngredient) {
+    public String swapIngredients(Ingredient pickedIngredient) {
+        String pickedIngredientName = pickedIngredient.getName();
         String exchangedIngredient = "";
 
         if (ingredientPairs.containsKey(pickedIngredient)) {
