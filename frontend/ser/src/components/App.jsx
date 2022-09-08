95a89b29cd39b92a73b4ef4047f53f5eba5ac7d5
elStriker <briankouophe38@gmail.com>
2022-09-08 14:09:43 +0200

    refactored frontend a while

--- frontend/ser/src/components/App.jsx
+++ frontend/ser/src/components/App.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import Header from './Header'
-import IngredientSelect from './IngredientSelect'
+import IngredientSelect from './IngredientSelect';
 import Sidebar from './Sidebar';
 import Overview from '../pages/Overview';
 import Swap from '../pages/Swap';
+import '../index.css';
 import {BrowserRouter as Router} from 'react-router-dom';
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom';
+import Header from './Header';
 
 function App() {
   return (
     <div className='App'>
       <>
         <Router>
+          <Header />
           <Sidebar />
           <Routes>
             <Route path='/' exact element={<Overview />} />
             <Route path='/swap' exact element={<Swap />} />
           </Routes>
         </Router>
-        <Header />
-          <IngredientSelect />
+        <IngredientSelect />
       </>
     </div>
   );
--- frontend/ser/src/components/IngredientSelect.jsx
+++ frontend/ser/src/components/IngredientSelect.jsx
@@ -14,8 +14,8 @@ export default function IngredientSelect() {
     },[]);
 
         return (
-            <FormControl sx={{minWidth:200, paddingLeft: 10}} className="select-container">
-                <InputLabel sx={{paddingLeft:10}}>Select your ingredient</InputLabel>
+            <FormControl sx={{minWidth:200, paddingLeft: 50}} className="select-container">
+                Select your Ingredient
                 <Select>
                     {ingredients.map ((ingredient) => 
                     <MenuItem key={ingredient.id}>
