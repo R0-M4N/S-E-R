6bf37612d59853a5eaecae8b9e8f6de40171f3d9
elStriker <briankouophe38@gmail.com>
2022-09-21 09:18:20 +0200

    working on swapAlgorithm

---
+++ backend/src/main/java/com/codecool/ser/data/IngredientCategory.java
@@ -0,0 +1,5 @@
+package com.codecool.ser.data;
+
+public enum IngredientCategory {
+    FRUIT, MEAT, VEGETABLE, DAIRY, NUT, FISH, BREAD
+}
--- backend/src/main/java/com/codecool/ser/persistence/repository/IngredientRepository.java
+++ backend/src/main/java/com/codecool/ser/persistence/repository/IngredientRepository.java
@@ -2,6 +2,8 @@ package com.codecool.ser.persistence.repository;
 
 import com.codecool.ser.persistence.entity.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 
 import java.util.List;
@@ -10,5 +12,9 @@ import java.util.List;
 public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
     List<Ingredient> findByProteinLessThanEqual(int amount);
     List<Ingredient> findByNameStartingWithIgnoreCaseAndProteinLessThanEqual(String name, int amount);
+
+    @Query("SELECT * FROM Ingredient WHERE protein BETWEEN :from and :to")
+    List<Ingredient> findAllIngredientByProtein(@Param("from") int from, @Param("to") int to);
+
     Ingredient findByName(String name);
 }
