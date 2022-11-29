b66a00499830c32c02607f91c84b5f134deb3e96
R0-M4N <fannroman850@gmail.com>
2022-11-29 14:21:39 +0100

    setup for security with JWT

--- backend/src/main/java/com/codecool/ser/SerApplication.java
+++ backend/src/main/java/com/codecool/ser/SerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SerApplication.class, args);
 	}
-
 }
