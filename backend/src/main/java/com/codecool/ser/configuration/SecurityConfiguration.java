d15974174a6e6a5c8f1eb786f5087749d20eb23b
R0-M4N <fannroman850@gmail.com>
2022-11-30 13:17:06 +0100

    jwtDecoder added, application.yml changed

--- backend/src/main/java/com/codecool/ser/configuration/SecurityConfiguration.java
+++ backend/src/main/java/com/codecool/ser/configuration/SecurityConfiguration.java
@@ -4,16 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    private final RsaKeyProperties keyProperties;
+
+    public SecurityConfiguration(RsaKeyProperties keyProperties) {
+        this.keyProperties = keyProperties;
+    }
 
     @Bean
     public InMemoryUserDetailsManager user() {
@@ -32,8 +42,14 @@ public class SecurityConfiguration {
                 .authorizeRequests(auth -> auth
                         .anyRequest().authenticated()
                 )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(withDefaults())
                 .build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(keyProperties.publicKey()).build();
     }
 }
